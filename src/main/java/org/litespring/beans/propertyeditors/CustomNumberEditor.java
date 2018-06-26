4af910e1c9697bebfe3d4082d22982a9713b80a4
prliwenbo <prwork@163.com>
2018-06-26 19:15:16 +0800

    qq

---
+++ src/main/java/org/litespring/beans/propertyeditors/CustomNumberEditor.java
@@ -0,0 +1,20 @@
+package org.litespring.beans.propertyeditors;/**
+ * Created by DELL on 2018/6/26.
+ */
+
+/**
+ * user is
+ **/
+
+
+public class CustomNumberEditor {
+    public CustomNumberEditor(Class<?> integerClass, boolean b) {
+    }
+
+    public void setAsText(String s) {
+    }
+
+    public Object getValue() {
+        return null;
+    }
+}
---
+++ src/test/java/org/litespring/test/v2/CustomNumberEditorTest.java
@@ -0,0 +1,35 @@
+package org.litespring.test.v2;/**
+ * Created by DELL on 2018/6/26.
+ */
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.litespring.beans.propertyeditors.CustomNumberEditor;
+
+/**
+ * user is lwb
+ **/
+
+
+public class CustomNumberEditorTest {
+
+    //CustomNumberEditor将字符串转换成数字类型，首先执行何种数字类型，int,double或其他数字类型，第二个参数指定是否对空或者null的字符串进行处理。
+    @Test
+    public void testConverString(){
+        CustomNumberEditor editor = new CustomNumberEditor(Integer.class,true);
+        editor.setAsText("3");
+        Object value = editor.getValue();
+        Assert.assertTrue(value instanceof Integer);
+        Assert.assertEquals(3,((Integer)editor.getValue()).intValue());
+
+        editor.setAsText("");
+        Assert.assertTrue(editor.getValue() == null);
+
+        try{
+            editor.setAsText("3.1");
+        }catch (Exception e){
+            return ;
+        }
+        Assert.fail();
+    }
+}
