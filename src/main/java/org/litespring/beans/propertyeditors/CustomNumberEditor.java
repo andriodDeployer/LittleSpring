269c9e6f4cb481f5b3e06a3334ae4c20c3e21a76
prliwenbo <prwork@163.com>
2018-06-27 15:43:50 +0800

    qq

--- src/main/java/org/litespring/beans/propertyeditors/CustomNumberEditor.java
+++ src/main/java/org/litespring/beans/propertyeditors/CustomNumberEditor.java
@@ -2,16 +2,18 @@ package org.litespring.beans.propertyeditors;/**
  * Created by DELL on 2018/6/26.
  */
 
+import org.litespring.util.NumberUtils;
 import org.litespring.util.StringUtils;
 
+import java.beans.PropertyEditorSupport;
 import java.text.NumberFormat;
 
 /**
  * user is
  **/
 
-
-public class CustomNumberEditor {
+//将string类型的数据转化成number类型数据，具体转换成何种number(number包含integer,double,float)，由targetClass指定。
+public class CustomNumberEditor extends PropertyEditorSupport{
     private final Class<? extends Number> numberClass;
     private final NumberFormat numberFormat;
     private final boolean allowEmpty;
@@ -28,21 +30,37 @@ public class CustomNumberEditor {
         this.allowEmpty = allowEmpty;
     }
 
-    public void setAsText(String text) {
-        if(this.allowEmpty && !StringUtils.hasText(text)){
-            setValue(null);
-        }else if(this.numberFormat != null){
-            //setValue(NumberU);
-        }
-    }
 
-    public Object getValue() {
-        return null;
-    }
-    public void setValue(Object value){
+    @Override
+    public void setValue(Object value) {
         if(value instanceof Number){
-
+            super.setValue(NumberUtils.convertNumberToTargetClass((Number) value,this.numberClass));
+        }else{
+            super.setValue(value);
         }
+    }
 
+    @Override
+    public void setAsText(String text) throws IllegalArgumentException {
+       if(this.allowEmpty && !StringUtils.hasText(text)) {
+           setValue(null);
+       }else if(this.numberFormat != null){
+           setValue(NumberUtils.parseNumber(text,this.numberClass,this.numberFormat));
+       }else{
+           setValue(NumberUtils.parseNumber(text,this.numberClass));
+       }
+    }
+
+    @Override
+    public String getAsText() {
+        Object value = getValue();
+        if(value == null){
+            return "";
+        }
+        if(numberFormat != null){
+            return numberFormat.format(value);
+        }else{
+            return value.toString();
+        }
     }
 }
