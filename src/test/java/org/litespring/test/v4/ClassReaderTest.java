3e0c1d3cc0af882fe081ada1ed7a006a1d7600ce
prliwenbo <prwork@163.com>
2018-07-17 16:21:24 +0800

    qq

---
+++ src/test/java/org/litespring/test/v4/ClassReaderTest.java
@@ -0,0 +1,31 @@
+package org.litespring.test.v4;
+/**
+ * Created by DELL on 2018/7/17.
+ */
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.litespring.core.io.ClassPathResource;
+import org.springframework.asm.ClassReader;
+
+import java.io.IOException;
+
+/**
+ * user is lwb
+ **/
+
+
+public class ClassReaderTest {
+
+    @Test
+    public void testGetClassMetaData() throws IOException {
+        ClassPathResource resource = new ClassPathResource("org/litespring/service/v4/PetStoreService.class");
+        ClassReader reader = new ClassReader(resource.getInputStream());
+
+        ClassMetadataReadingVisitor visitor = new ClassMetadataReadingVisitor();
+        reader.accept(visitor,ClassReader.SKIP_DEBUG);
+
+        //Assert.assertFalse(visitor.is);
+    }
+
+}
