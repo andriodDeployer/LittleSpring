7ac43fae36bceec9cc5b72a899146310f376fddc
prliwenbo <prwork@163.com>
2018-07-31 14:28:56 +0800

    qq

---
+++ src/test/java/org/litespring/dao/v5/AccountDao.java
@@ -0,0 +1,13 @@
+package org.litespring.dao.v5;/**
+ * Created by DELL on 2018/7/31.
+ */
+
+import org.litespring.stereotype.Component;
+
+/**
+ * user is lwb
+ **/
+
+@Component
+public class AccountDao {
+}
---
+++ src/test/java/org/litespring/service/v5/PetStoreService.java
@@ -0,0 +1,39 @@
+package org.litespring.service.v5;/**
+ * Created by DELL on 2018/7/31.
+ */
+
+import org.litespring.beans.factory.annotation.Autowired;
+import org.litespring.dao.v5.AccountDao;
+import org.litespring.dao.v5.ItemDao;
+import org.litespring.stereotype.Component;
+
+/**
+ * user is lwb
+ **/
+
+@Component(value = "petStore")
+public class PetStoreService {
+
+    @Autowired
+    private AccountDao accountDao;
+
+    @Autowired
+    private ItemDao itemDao;
+
+    public PetStoreService(){
+
+    }
+
+    public AccountDao getAccountDao() {
+        return accountDao;
+    }
+
+    public ItemDao getItemDao() {
+        return itemDao;
+    }
+
+
+    public void placeOrder(){
+        System.out.println("place order");
+    }
+}
